44b2c699d9c96e44105db718f2d3e98bfede2f33
Roman Kireev <50318432+kireevroi@users.noreply.github.com>
2022-08-24 16:24:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,46 +18,35 @@ database = db.db("Chatdb1")
 @bot.inline_handler(func=lambda c: True)
 def default_query(call):
     try:
-        cock = types.InlineQueryResultArticle('1', 'Cock Size',
-                                types.InputTextMessageContent(cockSize(call), parse_mode = 'MarkdownV2'),
-                                description = "Random cocksize generator",
+        cock = types.InlineQueryResultArticle('1', 'Brain',
+                                types.InputTextMessageContent(brainSize(call), parse_mode = 'MarkdownV2'),
+                                description = "Random brain generator",
                                 thumb_url = "https://dictionary.cambridge.org"
                                 "/ru/images/thumb/cocker_noun_002_07370.jpg?version=5.0.239:")
-        gay = types.InlineQueryResultArticle('2', 'Gay Percentage',
-                                types.InputTextMessageContent(gaySize(call), parse_mode = 'MarkdownV2'),
-                                description = "Random gay generator",
+        gay = types.InlineQueryResultArticle('2', 'Person Percentage',
+                                types.InputTextMessageContent(personSize(call), parse_mode = 'MarkdownV2'),
+                                description = "Random person generator",
                                 thumb_url = "https://upload.wikimedia.org/wikip"
                                 "edia/commons/thumb/1/12/LGBT_flag_square.svg/"
                                 "2048px-LGBT_flag_square.svg.png")
-#        bcock = types.InlineQueryResultArticle('3', 'Smallest cock',
-#                                types.InputTextMessageContent('Cock is good2'),
-#                                description = "Smallest cock in this chat",
-#                                thumb_url = "https://cs11.pikabu.ru/post_img"
-#                                "/big/2020/07/27/7/1595846674164769836.png")
-#        bgay = types.InlineQueryResultArticle('4', 'Gayest Gay',
-#                                types.InputTextMessageContent('Gay is perfect'),
-#                                description = "Gayest gay in the chat",
-#                                thumb_url = "https://pyxis.nymag.com/v1/imgs"
-#                                "/e1a/46e/df11faaaf2f297b5f608e9968bd9b8f124"
-#                                "-30-heman.rsquare.w330.jpg")
-        bot.answer_inline_query(call.id, [cock, gay], is_personal = True, cache_time = 3600 * 4)
+        bot.answer_inline_query(call.id, [brain, person], is_personal = True, cache_time = 3600 * 4)
     except Exception as e:
         print(e)
 
-def cockSize(message):
+def brainSize(message):
     try:
-        name_seq = ['Членес', 'Пенис', 'Пиструн', 'Писюн']
+        name_seq = ['Мозг', 'Думательная мышца', 'Череп']
         emoji_seq = ['\U0001F60A', '\U0001F60D', '\U0001F632', '\U0001F925']
         user = strReplace(message.from_user.username)
-        result = random.choice(name_seq)+' у @'+user+' *'+str(random.randint(0, 40))+'см*\. '+ random.choice(emoji_seq)
+        result = random.choice(name_seq)+' у @'+user+' *'+str(random.randint(0, 40))+'см в  обхвате*\. '+ random.choice(emoji_seq)
     except Exception as e:
         print(e)
     return result
-def gaySize(message):
+def personSize(message):
     try:
         chat_id = "AnyChat"
         user = strReplace(message.from_user.username)
-        result = '@' + user + ' сегодня гей на *'+str(random.randint(0, 100))+'%*\! \U0001F3F3\uFE0F\u200D\U0001F308'
+        result = '@' + user + ' сегодня человек на *'+str(random.randint(0, 100))+'%*\! \U0001F3F3\uFE0F\u200D\U0001F308'
     except Exception as e:
         print(e)
     return result
